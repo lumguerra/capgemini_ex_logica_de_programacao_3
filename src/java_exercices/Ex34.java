ee29d7b145c7818f5161a446a64584807bdeb59c
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-06-14 17:05:48 -0300

    calculo de media

---
+++ src/java_exercices/Ex34.java
@@ -0,0 +1,29 @@
+//34. Elabore um algoritmo que, dada a idade de um nadador. Classifique-o em uma das seguintes categorias:
+//a. Infantil A = 5 - 7 anos;
+//b. Infantil B = 8 - 10 anos;
+//c. Juvenil A = 11- 13 anos;
+//d. Juvenil B = 14 - 17 anos;
+//e. Sênior = 18 - 25 anos.
+//Apresentar mensagem “idade fora da faixa etária” quando for outro ano não contemplado;
+
+//
+
+
+package java_exercices;
+
+import java.util.Scanner;
+
+public class Ex34 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite a idade do nadador:");
+		int idade = scanner.nextInt();
+		scanner.nextLine();
+		
+		
+
+	}
+
+}
